624363cc7eb9e3cf9bda0146a8d7834b5436326b
aonori <aonori@aonr.jp>
2019-10-27 13:54:44 +0900

    Add API methods.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,7 +1,19 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
 #define DEBUG 1
 
 const int LED = 25;
 const int TEST_SW = 26;
+
+const int BLINK_COUNT = 5;
+
+// --- Wifi settings ---
+const char* ssid = "SSID";
+const char* password = "PASS";
+
+// --- Last Nage-Sen history API ---
+const char* host = "https://example.com";
 
 void setup() {
   pinMode(LED, OUTPUT_OPEN_DRAIN);
@@ -12,15 +24,31 @@ void setup() {
 void loop() {
   // Debug test mode
   if(DEBUG) {
-    digitalWrite(LED, !digitalRead(LED));
     if(digitalRead(TEST_SW) == LOW) {
-      delay(300); 
+      digitalWrite(LED, !digitalRead(LED));
+      delay(200); 
     }
     else {
-      delay(10);
+      digitalWrite(LED, LOW);
     }
+    return;
   }
 
   // Get Nage-Sen log records
-  
+  if(is_nagesen_payed()) {
+    for(int i=0; i < BLINK_COUNT * 2; i++) {
+      digitalWrite(LED, !digitalRead(LED));
+      delay(200);
+    }
+    // Activate LED
+    digitalWrite(LED, LOW);
+  }
+}
+
+bool is_nagesen_payed() {
+  HTTPClient http;
+  http.begin(host);
+  int httpCode = http.POST("");
+
+  return httpCode == 200;
 }
